b4a9f77e8eb003ed2e1685449b761544b228eb75
Leon Anavi <leon@anavi.org>
2016-03-29 00:29:22 +0000

    HTU21D: temperature and hummidity sensor
    Simple example written in the C programming
    language and based on wiringPi for reading
    temperature and hummidity from the i2c sensor
    HTU21D with address 40 on bus 1 of Raspberry Pi
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

---
+++ HTU21D/c/HTU21D.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "wiringPi.h"
+#include "wiringPiI2C.h"
+
+#define   ADDR   0x40
+
+// cTemp:
+//   Convert sensor reading into temperature.
+//   See page 14 of the datasheet
+
+double cTemp (int sensorTemp)
+{
+  double tSensorTemp = sensorTemp / 65536.0;
+  return -46.85 + (175.72 * tSensorTemp);
+}
+
+// cHumid:
+//   Convert sensor reading into humidity.
+//   See page 15 of the datasheet
+
+double cHumid (int sensorHumid)
+{
+  double tSensorHumid = sensorHumid / 65536.0;
+  return -6.0 + (125.0 * tSensorHumid);
+}
+
+int main (void)
+{
+  int fd;
+  unsigned int x;
+  unsigned char buf [4];
+  unsigned int temp, humid;
+
+  wiringPiSetup () ;
+
+  if ((fd = wiringPiI2CSetup (ADDR)) < 0)
+  {
+    fprintf (stderr, "Unable to open I2C device: %s\n", strerror (errno));
+    exit (-1);
+  }
+
+  // Temperature
+  wiringPiI2CWrite (fd, 0xF3);
+  delay (100);
+  x = read (fd, buf, 3);
+
+  if (x != 3)
+    printf ("%d: %02X %02X %02X\n", x, buf[0], buf[1], buf[2]);
+
+  temp = (buf [0] << 8 | buf [1]) & 0xFFFC;
+
+  printf("%5.2fC\n", cTemp(temp));
+
+  // Yoomidity
+
+  wiringPiI2CWrite(fd, 0xF5);
+  delay(100);
+  x = read(fd, buf, 3);
+
+  if (x != 3)
+    printf ("%d: %02X %02X %02X\n", x, buf[0], buf[1], buf[2]);
+
+  humid = (buf [0] << 8 | buf [1]) & 0xFFFC;
+
+  printf("%5.2f%%rh\n", cHumid(humid));
+
+  return 0;
+}
