8dec8df7709c67d492ecd3a57d1101d690f13d1c
Leon Anavi <leon@anavi.org>
2016-05-14 00:50:41 +0300

    BH1750: Split code into header file
    Add BH1750.h, refactor BH1750.c and create
    BH1750_test.c.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- BH1750/c/BH1750.c
+++ BH1750/c/BH1750.c
@@ -1,10 +1,5 @@
 #include <wiringPiI2C.h>
-#include <stdio.h>
-
-#define BH1750_ADDR 0x23
-
-//Delay getLux function
-#define LUXDELAY 500
+#include "BH1750.h"
 
 int getLux(int fd)
 {
@@ -13,11 +8,4 @@ int getLux(int fd)
 	int word = wiringPiI2CReadReg16(fd, 0x00);
 	int lux=((word & 0xff00)>>8) | ((word & 0x00ff)<<8);
 	return lux;
-}
-
-int main()
-{
-	int fd = wiringPiI2CSetup(BH1750_ADDR) ;
-	printf("Lux:%d \n", getLux(fd));
-	return 0 ;
 }
