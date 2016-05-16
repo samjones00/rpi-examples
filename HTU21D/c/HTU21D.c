3aab583b393b81edb9e7dba6e04aa686f798287c
Leon Anavi <leon@anavi.org>
2016-05-16 22:16:27 +0000

    HTU21D.c: Code refactoring
    Change functions, set all addresses as contants.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

--- HTU21D/c/HTU21D.c
+++ HTU21D/c/HTU21D.c
@@ -7,67 +7,51 @@
 #include "wiringPi.h"
 #include "wiringPiI2C.h"
 
-#define   ADDR   0x40
+#define   HTU21D_I2C_ADDR 0x40
 
-// cTemp:
-//   Convert sensor reading into temperature.
-//   See page 14 of the datasheet
+#define   HTU21D_TEMP     0xF3
+#define   HTU21D_HUMID    0xF5
 
-double cTemp (int sensorTemp)
+// Get temperature
+double getTemperature(int fd)
 {
-  double tSensorTemp = sensorTemp / 65536.0;
-  return -46.85 + (175.72 * tSensorTemp);
+	unsigned char buf [4];
+	wiringPiI2CWrite(fd, HTU21D_TEMP);
+	delay(100);
+	read(fd, buf, 3);
+	unsigned int temp = (buf [0] << 8 | buf [1]) & 0xFFFC;
+	// Convert sensor reading into temperature.
+	// See page 14 of the datasheet
+	double tSensorTemp = temp / 65536.0;
+	return -46.85 + (175.72 * tSensorTemp);
 }
 
-// cHumid:
-//   Convert sensor reading into humidity.
-//   See page 15 of the datasheet
-
-double cHumid (int sensorHumid)
+// Get humidity
+double getHumidity(int fd)
 {
-  double tSensorHumid = sensorHumid / 65536.0;
-  return -6.0 + (125.0 * tSensorHumid);
+	unsigned char buf [4];
+	wiringPiI2CWrite(fd, HTU21D_HUMID);
+	delay(100);
+	read(fd, buf, 3);
+  	unsigned int humid = (buf [0] << 8 | buf [1]) & 0xFFFC;
+	// Convert sensor reading into humidity.
+	// See page 15 of the datasheet
+	double tSensorHumid = humid / 65536.0;
+	return -6.0 + (125.0 * tSensorHumid);
 }
 
 int main (void)
 {
-  int fd;
-  unsigned int x;
-  unsigned char buf [4];
-  unsigned int temp, humid;
-
-  wiringPiSetup () ;
-
-  if ((fd = wiringPiI2CSetup (ADDR)) < 0)
-  {
-    fprintf (stderr, "Unable to open I2C device: %s\n", strerror (errno));
-    exit (-1);
-  }
-
-  // Temperature
-  wiringPiI2CWrite (fd, 0xF3);
-  delay (100);
-  x = read (fd, buf, 3);
-
-  if (x != 3)
-    printf ("%d: %02X %02X %02X\n", x, buf[0], buf[1], buf[2]);
-
-  temp = (buf [0] << 8 | buf [1]) & 0xFFFC;
-
-  printf("%5.2fC\n", cTemp(temp));
-
-  // Humidity
-
-  wiringPiI2CWrite(fd, 0xF5);
-  delay(100);
-  x = read(fd, buf, 3);
-
-  if (x != 3)
-    printf ("%d: %02X %02X %02X\n", x, buf[0], buf[1], buf[2]);
-
-  humid = (buf [0] << 8 | buf [1]) & 0xFFFC;
-
-  printf("%5.2f%%rh\n", cHumid(humid));
-
-  return 0;
+	wiringPiSetup();
+	int fd = wiringPiI2CSetup(HTU21D_I2C_ADDR);
+	if ( 0 > fd )
+	{
+		fprintf (stderr, "Unable to open I2C device: %s\n", strerror (errno));
+		exit (-1);
+	}
+	
+	printf("%5.2fC\n", getTemperature(fd));
+	printf("%5.2f%%rh\n", getHumidity(fd));
+	
+	return 0;
 }
