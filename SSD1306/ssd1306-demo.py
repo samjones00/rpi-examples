1c5d8ada21754aa1b4aa3107a745f3c46d747dcf
Leon Anavi <leon@anavi.org>
2020-01-13 01:15:50 +0200

    SSD1306: Simple Python 3 example
    Add simple Python 3 script for SSD1306 mini I2C
    OLED display using luma library.
    
    Signed-off-by: Leon Anavi <leon@anavi.org>

---
+++ SSD1306/ssd1306-demo.py
@@ -0,0 +1,24 @@
+# Install with:
+# sudo apt-get install -y python3-dev python3-pip libfreetype6-dev libjpeg-dev build-essential
+# sudo -H pip3 install --upgrade luma.oled
+# 
+# Run with:
+# 
+
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
+import time
+from PIL import ImageFont, ImageDraw
+
+serial = i2c(port=1, address=0x3C)
+device = ssd1306(serial, rotate=0)
+
+# Box and text rendered in portrait mode
+with canvas(device) as draw:
+    draw.rectangle(device.bounding_box, outline="white", fill="black")
+    font = ImageFont.load_default()
+    draw.text((10, 5), "Example", fill="white")
+    draw.text((15, 25), "Hello", fill="white")
+    draw.text((15, 35), "World", fill="white")
+input("Press Enter to exit...")
